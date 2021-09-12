ec0b214fce39fde316bcf179a7e1b37462decbd8
starseat <starseat3@gmail.com>
2021-09-12 21:59:06 +0900

    퍼블리싱 추가 (0910v2)

---
+++ 03.publishing/pc/publish/ui.js
@@ -0,0 +1,34 @@
+/** 퍼블리셔용 **/
+
+
+function tabAction(){
+    $('.btn_tab_cont').click( function() {
+        $('.btn_tab_cont').removeClass('tab_current');
+        $(this).addClass('tab_current');
+    } );
+}
+
+function mainvisualSlick(){
+    $('.main_visual_list').slick({
+        dots: false,
+        infinite: true,
+        speed: 150,
+        fade: true,
+        arrows:false,
+        autoplay: true,
+        cssEase: 'linear'
+    });
+}
+
+
+
+function listSlick(){
+    $('.slick_slide_list').slick({
+        dots: false,
+        arrows:true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+}
