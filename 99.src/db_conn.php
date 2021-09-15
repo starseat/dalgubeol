2a36d7519c8722fc3b244bbff16a8c7100fc29ce
starseat <starseat3@gmail.com>
2021-09-16 08:42:16 +0900

    퍼블리싱 최신화(0915, 이미지용량 줄임, 제품상세 추가)

--- 99.src/db_conn.php
+++ 99.src/db_conn.php
@@ -1,13 +1,14 @@
 <?php
     //header("Access-Control-Allow-Headers: X-Requested-With, X-Prototype-Version");
     
-	$servername = "127.0.0.1";
-	$username = "artgg_user";
-	$password = "artgg_pw";
-    $dbname = "artggdb";
-    $dbport = 3307;
+	$servername = "localhost";
+	$username = "dgb11";
+	$password = "ekfrnqjf112233";
+    $dbname = "dgb11";
+    // $dbport = 3306;
 
-	$conn = mysqli_connect($servername, $username, $password, $dbname, $dbport);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+	// $conn = mysqli_connect($servername, $username, $password, $dbname, $dbport);
 
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
