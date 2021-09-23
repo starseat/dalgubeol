be2ff878d686d2adb28c8e31b7caadc83057ba18
starseat <starseat3@gmail.com>
2021-09-23 18:48:50 +0900

    head 정보 변경

--- 99.src/m/fragment/head.php
+++ 99.src/m/fragment/head.php
@@ -3,21 +3,22 @@ ob_start(); // Initiate the output buffer
 ?>
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="desc">
     <title>달구벌명가식품</title>
-    <link rel="canonical" href="https://url">
+    <link rel="canonical" href="http://www.dalgubeolmakchang.com/m/index.php">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="title">
-    <meta property="og:description" content="deesc">
-    <meta property="og:url" content="url">
+    <meta property="og:title" content="달구벌명가식품">
+    <meta property="og:description" content="달구벌명가식품">
+    <meta property="og:url" content="http://www.dalgubeolmakchang.com/m/index.php">
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css"> -->
     <link rel="stylesheet" type="text/css" href="../public/vendor/slick-1.6.0/slick.css" media="all" />
     <link rel="stylesheet" type="text/css" href="../public/css/common_m.css" media="all" />
 </head>
